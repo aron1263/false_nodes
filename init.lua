c59e6d6a9fdc002253486e092f807d7ae1d9df3b
aron1263 <jag_0222@yahoo.com>
2013-06-26 21:06:14 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- aron1263 false nodes 6/23/2013
--- this is my first mod so if you are not satisfied pm me on minetestt: aron1263.
+-- this is my first mod.
 -- thanks for downloading!
 
 -- Nodes
@@ -66,54 +66,47 @@ minetest.register_node("false_nodes:trap_dirt", {
     climbable = false,
 })
 
+
 -- Crafting recipes!!!
 
 minetest.register_craft({
-	output = 'false_nodes:trap_grass',
-	recipe = {
-		{'', '', ''},
-		{'', 'default:dirt', ''},
-		{'', 'defult:stick', ''},
-        }
-	})
-minetest.register_craft({
 	output = 'false_nodes:trap_sand',
 	recipe = {
-		{'', '', ''},
-		{'default:sand', 'default:sand', 'default:sand'},
-		{'', '', ''},
-        }
+		{'default:glass', 'default:glass', 'default:glass'},
+		{'default:glass', 'default:sand', 'default:glass'},
+		{'default:glass', 'default:glass', 'default:glass'},
+ }
 	})
 minetest.register_craft({
 	output = 'false_nodes:trap_stone',
 	recipe = {
-		{'', '', ''},
-		{'default:stone', 'default:stone', 'default:stone'},
-		{'', '', ''},
+		{'default:glass', 'default:glass', 'default:glass'},
+		{'default:glass', 'default:stone', 'default:glass'},
+		{'default:glass', 'default:glass', 'default:glass'},
  }
 	})
 minetest.register_craft({
 	output = 'false_nodes:trap_cobble',
 	recipe = {
-		{'', '', ''},
-		{'default:cobble', 'default:cobble', 'default:cobble'},
-		{'', '', ''},
+		{'default:glass', 'default:glass', 'default:glass'},
+		{'default:glass', 'default:cobble', 'default:glass'},
+		{'default:glass', 'default:glass', 'default:glass'},
  }
 	})
 minetest.register_craft({
 	output = 'false_nodes:trap_desert_sand',
 	recipe = {
-		{'', '', ''},
-		{'default:desert_sand', 'default:desert_sand', 'default:desert_sand'},
-		{'', '', ''},
+		{'default:glass', 'default:glass', 'default:glass'},
+		{'default:glass', 'default:desert_sand', 'default:glass'},
+		{'default:glass', 'default:glass', 'default:glass'},
  }
 	})
 minetest.register_craft({
 	output = 'false_nodes:trap_dirt',
 	recipe = {
-		{'', '', ''},
-		{'default:dirt', 'default:dirt', 'default:dirt'},
-		{'', '', ''},
+		{'default:glass', 'default:glass', 'default:glass'},
+		{'default:glass', 'default:dirt', 'default:glass'},
+		{'default:glass', 'default:glass', 'default:glass'},
  }
 	})
 
